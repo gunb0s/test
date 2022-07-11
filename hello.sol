731de36109b1550c2b84d3a250fb59404babfdec
cain <cain.kim@lambda256.io>
2022-07-11 16:39:36 +0900

    abi

---
+++ hello.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract HelloSolidity {
+    constructor() {
+    }
+
+    function hello() public pure returns (uint) {
+        return 1;
+    }
+}
